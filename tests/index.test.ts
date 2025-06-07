d99bfde97dced3e91b399148e73ba8cc0aec37da
mjfwebb <michael@w3bb.net>
2025-06-07 22:43:06 +0200

    feat: add require-options-object

---
+++ tests/index.test.ts
@@ -0,0 +1,31 @@
+import { RuleTester } from "@typescript-eslint/rule-tester";
+import rule from "../src/index";
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("require-options-object", rule, {
+  valid: [
+    `function foo(a, b, c) { return a + b + c; }`,
+    `const bar = (a: number, b: string, c = 'default'): void => { console.log(a, b, c); }`,
+    `const baz = function (a, b, c) {};`,
+    // standard callback should be ignored
+    `['a', 'b'].replaceAll(x => x.toUpperCase());`,
+  ],
+  invalid: [
+    {
+      code: `function test(a, b, c, d) { return a + b + c + d; }`,
+      errors: [{ messageId: "requireOptions", data: { count: 4 } }],
+      output: `function test({ a, b, c, d }: { a: any; b: any; c: any; d: any }) { return a + b + c + d; }`,
+    },
+    {
+      code: `const fn = (x, y = 2, z, w = 'w') => x * y * z * parseInt(w);`,
+      errors: [{ messageId: "requireOptions", data: { count: 4 } }],
+      output: `const fn = ({ x, y = 2, z, w = 'w' }: { x: any; y?: number; z: any; w?: string }) => x * y * z * parseInt(w);`,
+    },
+    {
+      code: `const obj = { method(a, b, c, d) { return; } };`,
+      errors: [{ messageId: "requireOptions", data: { count: 4 } }],
+      output: `const obj = { method({ a, b, c, d }: { a: any; b: any; c: any; d: any }) { return; } };`,
+    },
+  ],
+});
